545f3b17eb523f2c44dbf5f0fc0efa12f73dfd08
jaggerabney <jagger.abney@gmail.com>
2023-06-22 09:44:31 -0700

    explored core components and styling

--- misc/react-native-demo/App.js
+++ misc/react-native-demo/App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello world!</Text>
+      <View>
+        <Text style={styles.text}>Hello again!</Text>
+      </View>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -17,5 +18,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "dashed",
   },
 });
