ceeeecdb3df093ec5f53f7df22812f690d64ca3a
jaggerabney <jagger.abney@gmail.com>
2023-06-22 10:20:37 -0700

    added flexbox deep dive; further explored flexbox

---
+++ misc/flexbox-deep-dive/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-around",
+        alignItems: "stretch",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "red",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: "blue",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: "green",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
+    </View>
+  );
+}
