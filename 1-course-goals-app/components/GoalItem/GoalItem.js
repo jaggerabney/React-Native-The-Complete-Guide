eb1aba6fcd61002e189e476706efa30498e5b057
jaggerabney <jagger.abney@gmail.com>
2023-06-27 11:24:17 -0700

    added ripple effect to items during deletion

--- 1-course-goals-app/components/GoalItem/GoalItem.js
+++ 1-course-goals-app/components/GoalItem/GoalItem.js
@@ -2,23 +2,30 @@ import { View, Text, StyleSheet, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDelete.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDelete.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
 
