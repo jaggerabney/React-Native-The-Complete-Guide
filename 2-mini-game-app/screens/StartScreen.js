53365c2acf6550bc7c4860b58c9fa73a94ebf5f4
jaggerabney <jagger.abney@gmail.com>
2023-06-28 10:09:12 -0700

    added styling to StartScreen

--- 2-mini-game-app/screens/StartScreen.js
+++ 2-mini-game-app/screens/StartScreen.js
@@ -1,15 +1,33 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton/PrimaryButton";
 
 function StartScreen() {
   return (
-    <View>
+    <View style={styles.inputWrapper}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
 
 export default StartScreen;
