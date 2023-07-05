e5160023c166397b3c56c90c4684e49e8068ef24
jaggerabney <jagger.abney@gmail.com>
2023-07-05 11:29:12 -0700

    added translucent background image

--- 2-mini-game-app/App.js
+++ 2-mini-game-app/App.js
@@ -1,12 +1,20 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartScreen from "./screens/StartScreen";
+import bgImage from "./assets/images/background.png";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.background}>
-      <StartScreen />
+      <ImageBackground
+        source={bgImage}
+        resizeMode="cover"
+        style={styles.background}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
