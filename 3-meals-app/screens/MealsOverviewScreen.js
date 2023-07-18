e6038317aa38d4fccab1a90462693aeef813aafb
jaggerabney <jagger.abney@gmail.com>
2023-07-18 10:37:01 -0700

    added content for MealItem component

--- 3-meals-app/screens/MealsOverviewScreen.js
+++ 3-meals-app/screens/MealsOverviewScreen.js
@@ -1,13 +1,25 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 
+import MealItem from "../components/MealItem/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
   const { categoryId } = route.params;
+  const meals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(categoryId) >= 0
+  );
+
+  function renderMealItem({ item }) {
+    return <MealItem title={item.title} />;
+  }
 
   return (
     <View styles={styles.container}>
-      <Text style={styles.text}>{`Meals Overview - ${categoryId}`}</Text>
+      <FlatList
+        data={meals}
+        keyExtractor={(meal) => meal.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
@@ -15,9 +27,7 @@ function MealsOverviewScreen({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  text: {
-    padding: 16,
+    margin: 16,
   },
 });
 
